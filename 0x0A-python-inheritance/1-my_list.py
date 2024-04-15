e9831297cc829f26b41193733336ee292c2fa35e
kess4obehi <kessiena4excellence@gmail.com>
2024-04-15 09:41:19 -0700

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" creating a subclass from a parent class called list"""
+
+class MyList(list):
+    """ A class that was inherited from List"""
+    def print_sorted(self):
+        """ A function that prints a sorted list"""
+        print(sorted(self))
