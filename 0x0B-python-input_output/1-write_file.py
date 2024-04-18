4bca740dc504494e367b85d95d2d6667b96aacb6
kess4obehi <kessiena4excellence@gmail.com>
2024-04-18 09:55:27 -0700

    writting into a file that returns the number of characters

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""creating a function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """
+    The created function that returns the number of characters written
+    """
+
+    with open(filename, "w", encoding="utf-8") as f
+    return f.write(text)
