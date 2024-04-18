4b1c87cb423e227fe2cc8caf0dbf2c2d2c0ac04d
kess4obehi <kessiena4excellence@gmail.com>
2024-04-18 10:11:11 -0700

    writting into a file that returns the number of characters

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""creating a function that writes a string to a text file"""
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
     """Write a string to a UTF"""
 
     with open(filename, "w", encoding="utf-8") as f:
-    return f.write(text)
+        return f.write(text)
