b2d778c6138d42dbe979609529c945ed4007defb
kess4obehi <kessiena4excellence@gmail.com>
2024-05-02 00:10:26 +0100

    creation of new files and tests folder

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A function that adds two integer or floats"""
+
+
+def add_integer(a, b=98):
+    """A function that returns the addition of two integers"""
+    if ((not isinstance(a, (int, float)))):
+        raise TypeError(a must be an integer)
+    if ((not isinstance(b, (int, float)))):
+        raise TypeError(a must be an integer)
+    return int(a) + int(b)
