e5b85e97e1da9705f9aeec0e09abe843c876fc61
kess4obehi <kessiena4excellence@gmail.com>
2024-05-02 00:32:33 +0100

    addition of newline at the end of file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@
 def add_integer(a, b=98):
     """A function that returns the addition of two integers"""
     if ((not isinstance(a, (int, float)))):
-        raise TypeError(a must be an integer)
+        raise TypeError("a must be an integer")
     if ((not isinstance(b, (int, float)))):
-        raise TypeError(a must be an integer)
-    return int(a) + int(b)
+        raise TypeError("a must be an integer")
+    return (int(a) + int(b))
