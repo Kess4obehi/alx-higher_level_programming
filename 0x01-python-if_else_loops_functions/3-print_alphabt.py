96cb6a7005fcdebde2537d71a469fde5ea8702b4
kess4obehi <kessiena4excellence@gmail.com>
2024-06-12 12:19:08 +0100

    new files created under if-else loops function

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/bin/usr/python3
+
+for m in range(97,123):
+    if (m != 101 and m != 113):
+        print("{}".format(chr(m)), end="")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for x in range(0, 98):
+    print("{} = {}".format(x, hex(x)))
+    
