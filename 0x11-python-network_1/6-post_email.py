bc3c33a763d5c11b07bd41aac2dffa538194a3d3
kess4obehi <kessiena4excellence@gmail.com>
2024-07-06 07:43:18 -0700

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A Python script that takes in a URL and an email addressand send post request"""
+"""A script that takes in a URL and an email addressand send post request"""
 
 import sys
 import requests
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     req = requests.post(url, ({"email": email}))
-    print("req.text")
+    print(req.text)
