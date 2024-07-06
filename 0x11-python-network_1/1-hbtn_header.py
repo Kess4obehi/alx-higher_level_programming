fcc7563309d9907a669084e4b29cb16f3a5e0f3d
kess4obehi <kessiena4excellence@gmail.com>
2024-07-05 20:36:51 -0700

    Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A Python script that takes in a URL, sends a request to the URL"""
+
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        head = req.headers
+        print(dict(head).get("X-Request-Id"))
